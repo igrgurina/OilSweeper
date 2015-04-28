915c934ab2214181c532c05cc27b406ea563b5ba
Ivan Grgurina <ivan.grgurina@gmail.com>
2015-04-28 21:22:32 +0200

    Added Engine.Maps namespace. Created tests for coordinate generation method. Comments.

---
+++ OilSweeper/OilSweeper.Engine/Maps/LocationGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OilSweeper.Engine.Maps
+{
+    /// <summary>
+    /// Class that contains method(s) for random coordinates generation.
+    /// </summary>
+    public static class LocationGenerator
+    {
+        /// <summary>
+        /// Generates multiple random coordinates.
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<ILocation> GenerateCoordinates()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OilSweeper/OilSweeper.Engine/Maps/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OilSweeper.Engine.Maps
+{
+    /// <summary>
+    /// Contains extension methods for ILocation.
+    /// </summary>
+    public static class Plugin
+    {
+        /// <summary>
+        /// Fethes static location images from Google Maps using REST.
+        /// </summary>
+        /// <param name="coordinates">ILocation object containing Latitude and Longitude.</param>
+        /// <returns></returns>
+        public static String FetchLocationImage(this ILocation coordinates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
