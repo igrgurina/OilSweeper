0ddfc819eb12bc95e1f4e607e1ae2e1686d9574d
Ivan Grgurina <ivan.grgurina@gmail.com>
2015-04-28 20:49:22 +0200

    ILocation interface.

--- OilSweeper/OilSweeper.Engine/ILocation.cs
+++ OilSweeper/OilSweeper.Engine/ILocation.cs
@@ -1,11 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace OilSweeper.Engine
+﻿namespace OilSweeper.Engine
 {
+    /// <summary>
+    /// Used to abstract coordinates from certain concrete implementations. 
+    /// </summary>
     public interface ILocation
     {
+        /// <summary>
+        /// Longitude, x-coordinate.
+        /// </summary>
+        double Longitude { get; set; }
+
+        /// <summary>
+        /// Latitude, y-coordinate.
+        /// </summary>
+        double Latitude { get; set; }
     }
 }
