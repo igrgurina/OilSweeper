2ba7ff6eb496dbf9c8865e25c3ba04c7b190f1ba
Grga Čvarak <grga.cvarak@mail.com>
2015-05-16 10:59:11 +0200

    MainScreen project
    MainScreen committed with Chapter, Slide and Question classes.

---
+++ MainScreen/MainScreen/Question.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainScreen
+{
+    class Question
+    {
+        private int questionID;
+        private string text;
+        private string image;
+        private List<string> options;
+        private string correct;
+
+        public Question(int questionID, string text, string image, List<string> options, string correct)
+        {
+            this.questionID = questionID;
+            this.text = text;
+            this.image = image;
+            this.options = options;
+            this.correct = correct;
+        }
+
+    }
+
+}
