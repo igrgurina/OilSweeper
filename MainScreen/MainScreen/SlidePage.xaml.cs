07f4756c7c426223d8e55fecc6eeb5bbd67d313e
Warbringer007 <daniman24@gmail.com>
2015-05-18 14:26:34 +0200

    Added references to images

--- MainScreen/MainScreen/SlidePage.xaml.cs
+++ MainScreen/MainScreen/SlidePage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Diagnostics;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 using MainScreen.ViewModel;
@@ -10,6 +12,11 @@ namespace MainScreen
         public SlidePage()
         {
             this.InitializeComponent();
+            this.ManipulationStarting += MainPage_ManipulationStarting;
+            this.ManipulationStarted += MainPage_ManipulationStarted;
+            this.ManipulationInertiaStarting += MainPage_ManipulationInertiaStarting;
+            this.ManipulationDelta += MainPage_ManipulationDelta;
+            this.ManipulationCompleted += MainPage_ManipulationCompleted;
         }
 
         /// <summary>
@@ -22,9 +29,41 @@ namespace MainScreen
             DataContext = (SlideViewModel)e.Parameter;
         }
 
-        private void Image_Tapped(object sender, TappedRoutedEventArgs e)
+        private void back_Image_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            this.Frame.Navigate(typeof(EducationPage));
+            Frame frame = Window.Current.Content as Frame;
+
+            if (frame != null && frame.CanGoBack)
+            {
+                frame.GoBack();
+                e.Handled = true;
+            }
         }
+
+        void MainPage_ManipulationStarting(object sender, ManipulationStartingRoutedEventArgs e)
+        {
+            Debug.WriteLine("MainPage_ManipulationStarting");
+        }
+        void MainPage_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            Debug.WriteLine("MainPage_ManipulationStarted");
+        }
+        void MainPage_ManipulationInertiaStarting(object sender, ManipulationInertiaStartingRoutedEventArgs e)
+        {
+            Debug.WriteLine("MainPage_ManipulationInertiaStarting");
+        }
+        void MainPage_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            Debug.WriteLine("MainPage_ManipulationDelta");
+        }
+        void MainPage_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+            Debug.WriteLine("MainPage_ManipulationCompleted");
+        }
+
+        /*private void UIElement_OnManipulationInertiaStarting(object sender, ManipulationInertiaStartingRoutedEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }*/
     }
 }
